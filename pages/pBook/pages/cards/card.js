7da5aa94f297204d6e3039252d7a90a6c6fc79b9
LIMENGYA <myjoyce817@163.com>
2019-02-15 17:54:13 +0800

    下载卡片功能

--- pages/pBook/pages/cards/card.js
+++ pages/pBook/pages/cards/card.js
@@ -8,6 +8,7 @@ Page({
         cards: [{}],
         current: 0,
         visible: false,
+        preurl:'',
     },
 
     onLoad(options) {
@@ -96,4 +97,163 @@ Page({
             this.setData({ cards })
         })
     },
+
+    /**下载卡片 */
+    saveCard() {
+        this.drawCanvas(); //调用绘图函数
+        wx.showLoading({
+            title: '努力生成中...'
+        });
+    },
+    /**
+     * 绘制图片和文本
+     */
+    drawCanvas(){
+        let {
+            cards,
+            current
+        } = this.data
+        var that = this;
+        //得到图片
+        let promise = new Promise(function(resolve,reject){
+            wx.getImageInfo({
+                src: cards[current].image,
+                success:function(res){
+                    resolve(res);
+                },
+                fail:function(res){
+                    reject(res);
+                }
+            })
+        }).then(res=>{
+            wx.downloadFile({  //网络图片需要先下载到本地
+                url: cards[current].image,
+                success(res){
+                    if(res.statusCode === 200){
+                        const ctx = wx.createCanvasContext('cardCanvas')
+                        ctx.setFillStyle('white')
+                        ctx.fillRect(0, 0, 297, 418)
+                        ctx.draw(true);
+                        that.roundRect(ctx, 0, 0, 297, 418, 10)
+                        ctx.drawImage(res.tempFilePath,10,10,280,206);
+                        ctx.draw(true)
+                        ctx.setTextAlign('left') 
+                        ctx.setFontSize(16)
+                        ctx.setFillStyle('black')
+                        const textHeight = that.fillTextWrap(ctx,that.data.cards[current].quote,7,245,270)
+                        ctx.fillText(that.data.cards[current].author[0], 258, 365)
+                        ctx.moveTo(222, 358)
+                        ctx.lineTo(257, 358)
+                        ctx.stroke()
+                        ctx.draw(true)
+                        ctx.draw(true,setTimeout(function(){
+                            wx.canvasToTempFilePath({
+                                x:0,
+                                y:0,
+                                width:297,
+                                height:418,
+                                destWidth:297,
+                                destHeight:418,
+                                canvasId:'cardCanvas',
+                                success:function(res){
+                                    console.log('res.tempFilePath'+res.tempFilePath);
+                                    that.data.preurl = res.tempFilePath;
+                                    wx.hideLoading()
+                                    that.saveToAlbum()
+                                },
+                                fail:function(res){
+                                    console.log(res);
+                                    
+                                }
+                            })
+                        },1000))
+                    }
+                }
+            })
+        }).catch(function(error){
+            console.log(error);
+            
+        })
+    },
+    /**
+     * 保存到相册
+     */
+    saveToAlbum:function(){
+        var that = this;
+        wx.getSetting({
+            success(res){
+                if(!res.authSetting['scope.writePhotoAlbum']){
+                    wx.authorize({
+                        scope:'scope.writePhotosAlbum',
+                        success(){
+                            that.startSaveImage()
+                        }
+                    })
+                } else {
+                    that.startSaveImage()
+                }
+            }
+        })
+    },
+
+    startSaveImage(){
+        let that = this;
+        wx.saveImageToPhotosAlbum({
+            filePath:that.data.preurl,
+            success(res){
+
+            }
+        })
+    },
+    // 文字换行
+    fillTextWrap(ctx, text, x, y, maxWidth, lineHeight) {
+        // 设定默认最大宽度
+        const systemInfo = wx.getSystemInfoSync();
+        const deciveWidth = systemInfo.screenWidth;
+        // 默认参数
+        maxWidth = maxWidth || deciveWidth;
+        lineHeight = lineHeight || 20;
+        // 校验参数
+        if (typeof text !== 'string' || typeof x !== 'number' || typeof y !== 'number') {
+            return;
+        }
+        // 字符串分割为数组
+        const arrText = text.split('');
+        // 当前字符串及宽度
+        let currentText = '';
+        let currentWidth;
+        for (let letter of arrText) {
+            currentText += letter;
+            currentWidth = ctx.measureText(currentText).width;
+            if (currentWidth > maxWidth) {
+                ctx.fillText(currentText, x, y);
+                currentText = '';
+                y += lineHeight;
+            }
+        }
+        if (currentText) {
+            ctx.fillText(currentText, x, y);
+        }
+    },
+    /**绘制圆角边框 */
+    roundRect(ctx,x,y,w,h,r){
+        ctx.save();
+        if(w<2*r){
+            r = w / 2;
+        }
+        if(h < 2*r){
+            r = h / 2;
+        }
+        ctx.beginPath();
+        ctx.setStrokeStyle('grey');
+        ctx.setLineWidth(1);
+        ctx.moveTo(x + r,y);
+        ctx.arcTo(x+w,y,x+w,y+h,r);
+        ctx.arcTo(x+w,y+h,x,y+h,r);
+        ctx.arcTo(x,y+h,x,y,r);
+        ctx.arcTo(x,y,x+w,y,r);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.draw(true);
+    }
 })
